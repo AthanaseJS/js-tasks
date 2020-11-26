6c811f94cbbdd88168db3b40ee08ef5c9f6f9947
Athanase <amakaridi@gmail.com>
2020-11-27 00:48:25 +0400

    etghe

---
+++ cars_js/vanila.js
@@ -0,0 +1,22 @@
+const cars = [
+  { name: 'Volvo V70', price: '60000', img: '/cars_js/img/Volvo.jpg' },
+
+  { name: 'BMW M3', price: '40000', img: '/cars_js/img/BMW.jpg' },
+  { name: 'Bugaty Verona', price: '70000', img: '/cars_js/img/Bujaty.jpg' },
+  { name: 'Ford Mustang', price: '25000', img: '/cars_js/img/Ford.jpg' },
+]
+
+function createCard(car) {
+  return
+  ;<div class="card">
+    <div class="card-img">
+      <img src="${car.img}" alt="${car.name}" />
+    </div>
+    <h3>${car.name}</h3>
+    <p>${car.price}</p>
+  </div>
+}
+
+const templatwe = cars.map((car) => createCard(car))
+const html = templatwe.join(' ')
+document.querySelector('.list').innerHTML = html
