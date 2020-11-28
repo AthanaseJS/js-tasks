aa5627ee9e142aeabbb5bfdb6b4e434a32b76e94
Athanase <amakaridi@gmail.com>
2020-11-28 19:23:06 +0400

    erg

--- cars_js/vanilla.js
+++ cars_js/vanilla.js
@@ -1,3 +1,6 @@
+var fruits = ['Banana', 'Orange', 'Apple', 'Mango']
+document.getElementById('demo1').innerHTML = fruits
+
 const cars = [
   { name: 'Volvo V70', price: '60000', img: '/cars_js/img/Volvo.jpg' },
   { name: 'BMW M3', price: '40000', img: '/cars_js/img/BMW.jpg' },
@@ -6,16 +9,16 @@ const cars = [
 ]
 
 function createCard(car) {
-  return
-  ;<div class="card">
+  return `
+  <div class="card">
     <div class="card-img">
       <img src="${car.img}" alt="${car.name}" />
     </div>
     <h3>${car.name}</h3>
     <p>${car.price}</p>
-  </div>
+  </div>`
 }
 
-const templatwe = cars.map((car) => createCard(car))
-const html = templatwe.join(' ')
+const templates = cars.map((car) => createCard(car))
+const html = templates.join(' ')
 document.querySelector('.list').innerHTML = html
